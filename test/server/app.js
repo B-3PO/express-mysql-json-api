efc38c330cd780e18e3548a4eccfae5d69ca26c9
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2016-08-01 21:15:41 -0500

    update

--- test/server/app.js
+++ test/server/app.js
@@ -77,68 +77,35 @@ dataManager.addType({
   }
 });
 
-
 dataManager.addType({
-  name: 'rooms',
-  table: 'rooms',
+  name: 'menus',
+  table: 'menus',
   attributes: {
-    name: {dataType: dataManager.dataType.STRING}
+    name: {dataType: dataManager.dataType.STRING},
+    type: {dataType: dataManager.dataType.STRING}
   }
 });
 
 dataManager.addType({
-  name: 'people',
-  table: 'people',
+  name: 'categories',
+  table: 'categories',
   attributes: {
-    name: {
-      dataType: 'string',
-      build: [
-        {field: 'first', dataType: dataManager.dataType.STRING},
-        {join: ' '},
-        {field: 'last', dataType: dataManager.dataType.STRING}
-      ]
-    },
-    age: {dataType: dataManager.dataType.INT},
-    email: {dataType: dataManager.dataType.STRING},
-    working: {dataType: dataManager.dataType.BOOLEAN}
-  }
-});
-
-dataManager.addType({
-  name: 'tester',
-  table: 'tester',
-  attributes: {
-    name: {dataType: dataManager.dataType.STRING}
+    name: {dataType: dataManager.dataType.STRING},
+    type: {dataType: dataManager.dataType.STRING}
   }
 });
 
 
-dataManager.addType({
-  name: 'jobs',
-  table: 'jobs',
-  attributes: {
-    title: {dataType: dataManager.dataType.STRING},
-    pay: {dataType: dataManager.dataType.CURRENCY}
-  }
-});
+
+
 
 
 var locationsManager = dataManager.CreateResource({
   name: 'locations',
   type: 'locations',
   relationships: {
-    people: {
-      resource: 'people',
-      manyToMany: true
-    },
-    rooms: {
-      resource: 'rooms',
-      oneToMany: true,
-      // constraint: true,
-      field: 'locations_id'
-    },
-    tester: {
-      resource: 'tester',
+    menus: {
+      resource: 'menus',
       manyToMany: true
     }
   }
@@ -147,152 +114,22 @@ app.use('/locations', locationsManager);
 
 
 
-app.use('/rooms', dataManager.CreateResource({
-  name: 'rooms',
-  type: 'rooms'
-}));
-
-app.use('/people', dataManager.CreateResource({
-  name: 'people',
-  type: 'people',
+var menusManager = dataManager.CreateResource({
+  name: 'menus',
+  type: 'menus',
   relationships: {
-    job: {
-      resource: 'job',
-      field: 'job'
-    },
-    tester: {
-      resource: 'tester',
+    categories: {
+      resource: 'categories',
       manyToMany: true
     }
   }
-}));
-
-app.use('/job', dataManager.CreateResource({
-  name: 'job',
-  type: 'jobs'
-}));
-
-
-var testerManager = dataManager.CreateResource({
-  name: 'tester',
-  type: 'tester'
 });
-app.use('/tester', testerManager);
-
-testerManager.onCreate(function (req, res) {
-  // console.log(req.body);
-  //
-  // var query = 'upda users.first,users.last,userscope.admin,userscope.organization_id,userscope.venue_id from users';
-  // query += ' left join userscope on userscope.user_id = users.id';
-  // query += ' where email=\'' + email + '\' and password=\'' + password + '\'';
-
-  // connection.query(query, function(error, rows, fields) {
-  //   if (error) {
-  //     return;
-  //   }
-  // });
+app.use('/menus', menusManager);
 
 
-  res.end();
+
+var cateogriesManager = dataManager.CreateResource({
+  name: 'categories',
+  type: 'categories'
 });
-testerManager.onRelationUpdate(function (req, res) {
-  res.end();
-});
-
-
-//
-//
-//
-// dataManager.addType({
-//   name: 'locations',
-//   table: 'locations',
-//   attributes: {
-//     name: {dataType: dataManager.dataType.STRING},
-//     city: {dataType: dataManager.dataType.STRING},
-//     state: {dataType: dataManager.dataType.STRING}
-//   },
-//   relationships: [
-//     {
-//       type: 'people',
-//       manyToMany: true
-//     },
-//     {
-//       type: 'rooms',
-//       oneToMany: true
-//     }
-//   ]
-// });
-//
-//
-// dataManager.addType({
-//   name: 'rooms',
-//   table: 'rooms',
-//   constraint: true,
-//   attributes: {
-//     name: {dataType: dataManager.dataType.STRING}
-//   }
-// });
-//
-// dataManager.addType({
-//   name: 'people',
-//   table: 'people',
-//   constraint: true,
-//   attributes: {
-//     name: {
-//       dataType: 'string',
-//       build: [
-//         {field: 'first', dataType: dataManager.dataType.STRING},
-//         {join: ' '},
-//         {field: 'last', dataType: dataManager.dataType.STRING}
-//       ]
-//     },
-//     age: {dataType: dataManager.dataType.INT},
-//     email: {dataType: dataManager.dataType.STRING},
-//     working: {dataType: dataManager.dataType.BOOLEAN}
-//   },
-//   relationships: [
-//     {
-//       type: 'jobs',
-//       field: 'job',
-//       single: true
-//     }
-//   ]
-// });
-//
-// dataManager.addType({
-//   name: 'jobs',
-//   table: 'jobs',
-//   attributes: {
-//     title: {dataType: dataManager.dataType.STRING},
-//     pay: {dataType: dataManager.dataType.CURRENCY}
-//   }
-// });
-//
-//
-//
-// app.use('/locations', dataManager.CreateResource({
-//   name: 'locations',
-//   type: 'locations',
-//   relationships: {
-//     people: {resource: 'people'},
-//     rooms: {resource: 'rooms'}
-//   }
-// }));
-//
-// app.use('/rooms', dataManager.CreateResource({
-//   name: 'rooms',
-//   type: 'rooms'
-// }));
-//
-// app.use('/people', dataManager.CreateResource({
-//   name: 'people',
-//   type: 'people',
-//   relationships: {
-//     job: {resource: 'job'}
-//   }
-// }));
-//
-// app.use('/job', dataManager.CreateResource({
-//   name: 'job',
-//   type: 'jobs'
-// }));
+app.use('/categories', cateogriesManager);
